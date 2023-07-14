da9c1603fabe5694f1c3436eef1429a7f2ab7b6d
Tushar@8640 <imtantushar44@gmail.com>
2023-07-15 02:49:20 +0600

    book details page added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,10 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
 import Login from "@/pages/Login";
 import Home from "@/pages/Home";
-import SignUp from "@/pages/SignUp";
 import NotFound from "@/pages/NotFound";
 import Books from "@/pages/Books";
 import BookDetails from "@/pages/BookDetails";
+import Signup from "@/pages/Signup";
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -31,7 +31,7 @@ const routes = createBrowserRouter([
   },
   {
     path: "/signup",
-    element: <SignUp />,
+    element: <Signup />,
   },
   {
     path: "*",
