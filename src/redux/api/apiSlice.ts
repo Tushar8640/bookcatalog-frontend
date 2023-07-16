fc9f253473675121a9a402d128b3140db6dc07fa
Tushar@8640 <imtantushar44@gmail.com>
2023-07-16 20:29:56 +0600

    favicon complete

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,8 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1' }),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalogue-eight.vercel.app/api/v1",
+  }),
   endpoints: () => ({}),
-  tagTypes: ['Books',"wishlist","readlist","review"],
+  tagTypes: ["Books", "wishlist", "readlist", "review"],
 });
